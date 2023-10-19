72252a3af40a2ffc7e2c631187eebb749d1dd88a
Kaiyu Yang <kaiyuy@caltech.edu>
2023-10-18 20:15:04 -0700

    work with gpu

--- lakefile.lean
+++ lakefile.lean
@@ -2,13 +2,12 @@ import Lake
 open Lake DSL
 
 package «miniF2F-lean4» {
-  moreLinkArgs := #["-L./lake-packages/LeanInfer/build/lib", "-L/usr/local/lib", "-lonnxruntime", "-lctranslate2", "-lstdc++"]
+  moreLinkArgs := #["-L./lake-packages/LeanInfer/build/lib", "-lonnxruntime", "-lctranslate2"]
 }
 
 
-require aesop from git "https://github.com/JLimperg/aesop" @ "master"
-require LeanInfer from git "https://github.com/lean-dojo/LeanInfer" @ "improve-backend"
 require mathlib from git "https://github.com/leanprover-community/mathlib4"
+require LeanInfer from git "https://github.com/lean-dojo/LeanInfer" @ "improve-backend"
 
 @[default_target]
 lean_lib «MiniF2F» {
