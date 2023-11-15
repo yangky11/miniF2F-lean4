bbd00611e418f6fc0516035a6a81d6cf09089574
Kaiyu Yang <kaiyuy@caltech.edu>
2023-11-15 14:07:26 -0800

    ~

--- MiniF2F/Valid.lean
+++ MiniF2F/Valid.lean
@@ -21,7 +21,6 @@ set_option trace.aesop.profile true
 
 open BigOperators Real Nat Topology
 
-
 theorem amc12a_2019_p21 (z : ℂ) (h₀ : z = (1 + Complex.I) / Real.sqrt 2) :
   ((∑ k : ℤ in Finset.Icc 1 12, z ^ k ^ 2) * (∑ k : ℤ in Finset.Icc 1 12, 1 / z ^ k ^ 2)) = 36 := by
   sorry
