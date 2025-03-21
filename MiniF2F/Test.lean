f1b56606f92bb53ac6ec26efce85a04563292ee8
Marcin Wrochna <mwrochna@gmail.com>
2025-03-21 12:45:32 +0100

    Bump to mathlib 4.17.0.

--- MiniF2F/Test.lean
+++ MiniF2F/Test.lean
@@ -955,7 +955,7 @@ theorem amc12a_2020_p15
   (a b : ℂ)
   (h₀ : a^3 - 8 = 0)
   (h₁ : b^3 - 8 * b^2 - 8 * b + 64 = 0) :
-  Complex.abs (a - b) ≤ 2 * Real.sqrt 21 := by sorry
+  ‖a - b‖ ≤ 2 * Real.sqrt 21 := by sorry
 
 theorem mathd_algebra_332
   (x y : ℝ)
